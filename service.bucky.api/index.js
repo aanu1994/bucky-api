ead8aca0d7f58db4cbf54ee48f54b9bb58411733
Tobi Adekanye <tobiadekanye@Tobis-MacBook-Pro.local>
2019-12-26 06:45:44 -0500

    BKY-2 - Updating index.js to console log out all of the requests being made to the api

--- service.bucky.api/index.js
+++ service.bucky.api/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`);
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('Welcome to the bucky api!!!!');
 });
